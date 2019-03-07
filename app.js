cb3fb1670cc96b76d01397bf6531dafb8d5a8213
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-03-06 20:35:48 -0600

    patricks Changes

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 
 
-$.ajax({
-    url: "http://localhost:3000/api/tables/",
-    context: document.body
-  }).done(function() {
-    // $( this ).addClass( "done" );
-  });
+
+$.get("/api/reservation", function(data){
+for(let i = 0; i< data.length; i++){
+    console.log(data[i]);
+
+    let reservedTable = $("<li class='tables-reserved'>");
+
+    reservedTable.append($("<h2>").text("customerName: " + data[i].name));
+    reservedTable.append($("<h2>").text("phoneNumber: " + data[i].number));
+    reservedTable.append($("<h2>").text("customerEmail: " + data[i].email));
+    reservedTable.append($("<h2>").text("customerID: " + data[i].id));
+
+
+}
+})
+
+$.get("/api/waitlist", function(data){
+    for(let i = 0; i< data.length; i++){
+        console.log(data[i]);
+
+        let waitList = $("<li class='wait-list'>");
+
+        waitList .append($("<h2>").text("customerName: " + data[i].name));
+        waitList .append($("<h2>").text("phoneNumber: " + data[i].number));
+        waitList .append($("<h2>").text("customerEmail: " + data[i].email));
+        waitList .append($("<h2>").text("customerID: " + data[i].id));
+    
+    
+    }
+    })
+
 
 
 
--- server.js
+++ server.js
@@ -1,24 +1,56 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
+app.get("/", function (req, res){
+  res.sendFile(path.join(__dirname, 'home.html'));
+})
 app.get("/home", function (req, res){
   res.sendFile(path.join(__dirname, 'home.html'));
 })
 app.get("/tables", function (req, res){
   res.sendFile(path.join(__dirname, 'tables.html'));
 })
-app.get("/reserve", function (req, res){
+app.get("/reservation", function (req, res){
   res.sendFile(path.join(__dirname, 'reserve.html'));
 })
+
+app.get("/waitlist", function (req, res){
+  res.sendFile(path.join(__dirname, 'reserve.html'));
+})
+
+let people = [];
+
+
+app.post("/api/reserve ", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var reserveTabel = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  reserveTabel.routeName = reserveTabel.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(reserveTabel);
+
+people.push(reserveTabel);
+
+  res.json(reserveTabel);
+});
+
 
 
 app.listen(PORT, function() {
   console.log('App listening on PORT ' + PORT);
 });
+
+
 
 
