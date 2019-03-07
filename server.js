791f6653396ad66644ec8c16de3a6598e7c4b146
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-03-06 19:43:24 -0600

    all the goodies

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-abstract.listen(PORT, function() {
+
+
+
+app.get("/home", function (req, res){
+  res.sendFile(path.join(__dirname, 'home.html'));
+})
+app.get("/tables", function (req, res){
+  res.sendFile(path.join(__dirname, 'tables.html'));
+})
+app.get("/reserve", function (req, res){
+  res.sendFile(path.join(__dirname, 'reserve.html'));
+})
+
+
+app.listen(PORT, function() {
   console.log('App listening on PORT ' + PORT);
 });
+
+
