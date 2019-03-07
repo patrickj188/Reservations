6a9482bb6b34a493711c9457d53c20e8d7f56b92
Kyle Laster <kyle.laster@gmail.com>
2019-03-06 20:12:49 -0600

    data

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const data = require('data');
+const data = require('./data');
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +13,8 @@ app.get('/tables', function(req, res) {
 app.get('/reserve', function(req, res) {
   res.sendFile(path.join(__dirname, 'reserve.html'));
 });
+
+app.post('');
 
 app.listen(PORT, function() {
   console.log('App listening on PORT ' + PORT);
